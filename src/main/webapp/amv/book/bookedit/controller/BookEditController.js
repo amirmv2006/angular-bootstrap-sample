d46576785dcf07df01d94f3cc3b6152ff6d2697b
Amir MV <amirmv2006@gmail.com>
2017-04-12 17:39:41 +0800

    -adding edit and delete actions

--- src/main/webapp/amv/book/bookedit/controller/BookEditController.js
+++ src/main/webapp/amv/book/bookedit/controller/BookEditController.js
@@ -15,7 +15,15 @@ angular.module('Book')
         }));
         console.log("initializing Book model");
         main.bookForm = {};
+
         main.book = {};
+
+        var id = $location.search().id;
+        if (id) {
+            $http.get('cxf/rest/Book/' + id).then(function (result) {
+                angular.copy(result.data, main.book);
+            });
+        }
 
         main.resetForm = function () {
             main.book.name = null;
@@ -26,14 +34,25 @@ angular.module('Book')
         main.save = function () {
             console.log(main.book);
             // if (main.bookForm.$valid) {
-                $http.post('cxf/rest/Book', angular.copy(main.book)).then(function (result) {
-                    main.resetForm();
-                    $log.debug('RESULT', result);
-                    MainPageService.goBack();
-                }, function (reason) {
-                    NotificationService.addErrorNotification(reason.data.detailMessage);
-                    $log.debug('ERROR', reason);
-                });
+                if (main.book.id) {
+                    $http.put('cxf/rest/Book', angular.copy(main.book)).then(function (result) {
+                        main.resetForm();
+                        $log.debug('RESULT', result);
+                        MainPageService.goBack();
+                    }, function (reason) {
+                        NotificationService.addErrorNotification(reason.data.detailMessage);
+                        $log.debug('ERROR', reason);
+                    });
+                } else {
+                    $http.post('cxf/rest/Book', angular.copy(main.book)).then(function (result) {
+                        main.resetForm();
+                        $log.debug('RESULT', result);
+                        MainPageService.goBack();
+                    }, function (reason) {
+                        NotificationService.addErrorNotification(reason.data.detailMessage);
+                        $log.debug('ERROR', reason);
+                    });
+                }
             // } else {
             //     console.log('Invalid');
             // }
--- src/main/webapp/amv/book/booklist/controller/BookListController.js
+++ src/main/webapp/amv/book/booklist/controller/BookListController.js
@@ -1,88 +1,145 @@
 //noinspection JSAnnotator
 angular.module('Book')
-    .controller('BookListPageCtrl', function ($scope, $location, $controller, MainPageService, Page, PageAction, DTOptionsBuilder, DTColumnBuilder, $http, $q) {
-        var main = this;
-        main = angular.extend(main, $controller('BasePageCtrl', {
-            $scope: $scope,
-            $controller: $controller,
-            MainPageService: MainPageService,
-            pageName: "BookList"
-        }));
-        var bookListPage = MainPageService.findPage("BookList");
-        bookListPage.addAction(new PageAction("Add", "fa fa-plus", function () {
-            $location.path('/book/add');
-        }));
-        bookListPage.addAction(new PageAction("Search", "fa fa-search", function () {
-            main.dtInstance._renderer.rerender();
-        }));
-        main.dtOptions = DTOptionsBuilder
-        /*.newOptions().withOption('ajax', {
-         url: 'cxf/rest/Book/datatable',
-         dataType: "jsonp",
-         type: 'POST'
-         // data: function (data, dtInstance) {
-         //     console.log(data);
-         //     // Modify the data object properties here before being passed to the server
-         // }
-         }).withDataProp('data')*/
+    .controller('BookListPageCtrl', function ($compile, $scope, $location, $controller, $document, $uibModal, MainPageService, Page, PageAction, DTOptionsBuilder, DTColumnBuilder, $http, $q) {
+            var main = this;
+            main = angular.extend(main, $controller('BasePageCtrl', {
+                $scope: $scope,
+                $controller: $controller,
+                MainPageService: MainPageService,
+                pageName: "BookList"
+            }));
+            var bookListPage = MainPageService.findPage("BookList");
+            bookListPage.addAction(new PageAction("Add", "fa fa-plus", function () {
+                $location.path('/book/add');
+            }));
+            bookListPage.addAction(new PageAction("Search", "fa fa-search", function () {
+                main.dtInstance._renderer.rerender();
+            }));
+            main.dtOptions = DTOptionsBuilder
+            /*.newOptions().withOption('ajax', {
+             url: 'cxf/rest/Book/datatable',
+             dataType: "jsonp",
+             type: 'POST'
+             // data: function (data, dtInstance) {
+             //     console.log(data);
+             //     // Modify the data object properties here before being passed to the server
+             // }
+             }).withDataProp('data')*/
 
-            .newOptions().withFnServerData(function (sSource, aoData, fnCallback, oSettings) {
-                    var pagingDto = {
-                        pageNumber: aoData[3].value / aoData[4].value,
-                        pageSize: aoData[4].value,
-                    };
-                    var orderList = [];
-                    for (var orderIndex = 0; orderIndex < aoData[2].value.length; orderIndex++) {
-                        var orderColumnIndex = aoData[2].value[orderIndex].column;
-                        var orderPropertyName = aoData[1].value[orderColumnIndex].data;
-                        var sortDto = {
-                            propertyName: orderPropertyName,
-                            ascending: aoData[2].value[orderIndex].dir.toLowerCase() == "asc"
+                .newOptions().withFnServerData(function (sSource, aoData, fnCallback, oSettings) {
+                        var pagingDto = {
+                            pageNumber: aoData[3].value / aoData[4].value,
+                            pageSize: aoData[4].value,
                         };
-                        orderList.unshift(sortDto);
+                        var orderList = [];
+                        for (var orderIndex = 0; orderIndex < aoData[2].value.length; orderIndex++) {
+                            var orderColumnIndex = aoData[2].value[orderIndex].column;
+                            var orderPropertyName = aoData[1].value[orderColumnIndex].data;
+                            var sortDto = {
+                                propertyName: orderPropertyName,
+                                ascending: aoData[2].value[orderIndex].dir.toLowerCase() == "asc"
+                            };
+                            orderList.unshift(sortDto);
+                        }
+                        pagingDto.sortList = orderList;
+
+                        $http.post('cxf/rest/Book/searchByExamplePaged',
+                            {
+                                pagingDto: pagingDto,
+                                example: main.searchObject
+                            }).then(function (result) {
+                            const records = {
+                                draw: aoData[0],
+                                recordsTotal: result.data.totalCount,
+                                recordsFiltered: result.data.afterFilterCount,
+                                // recordsFiltered: result.headers('x-meta-total'),
+                                data: result.data.records
+                            };
+                            fnCallback(records);
+                        })
+
                     }
-                    pagingDto.sortList = orderList;
+                )
 
-                    $http.post('cxf/rest/Book/searchByExamplePaged',
-                        {
-                            pagingDto: pagingDto,
-                            example:main.searchObject
-                        }).then(function (result) {
-                        const records = {
-                            draw: aoData[0],
-                            recordsTotal: result.data.totalCount,
-                            recordsFiltered: result.data.afterFilterCount,
-                            // recordsFiltered: result.headers('x-meta-total'),
-                            data: result.data.records
-                        };
-                        fnCallback(records);
-                    })
+                // .fromFnPromise(function (p1, p2, p3, p4) {
+                //     var defer = $q.defer();
+                //     $http.get('cxf/rest/Book').then(function (result) {
+                //         defer.resolve(result.data);
+                //     });
+                //     return defer.promise;
+                // })
+                .withPaginationType('full_numbers')
+                .withOption('bFilter', false)
+                .withOption('serverSide', true);
 
-                }
-            )
+            main.dtInstance = {};
 
-            // .fromFnPromise(function (p1, p2, p3, p4) {
-            //     var defer = $q.defer();
-            //     $http.get('cxf/rest/Book').then(function (result) {
-            //         defer.resolve(result.data);
-            //     });
-            //     return defer.promise;
-            // })
-            .withPaginationType('full_numbers')
-            .withOption('bFilter', false)
-            .withOption('serverSide', true);
+            main.dtColumns = [
+                DTColumnBuilder.newColumn('name').withTitle('Name').withOption('defaultContent', '-'),
+                DTColumnBuilder.newColumn('publishYear').withTitle('Publish Year'),
+                DTColumnBuilder.newColumn('id').withTitle('-').withOption('render', function (data, type, full) {
+                    return '<a href="javascript:;" class="ng-scope" ng-click="bookListPageCtrl.edit(' + data + ')"><span class="fa fa-pencil"></span></a>&nbsp;' +
+                        '<a href="javascript:;" class="ng-scope" ng-click="bookListPageCtrl.delete(' + data + ')"><span class="fa fa-trash"></span></a>'
+                })
+            ];
 
-        main.dtInstance = {};
+            main.edit = function (id) {
+                $location.path('/book/add').search({id: id});
+            };
+            main.delete = function (id) {
+                var parentElem = angular.element($document[0].querySelector('.modal-demo'));
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'DeleteConfirm.html',
+                    controller: 'DeleteModelCtrl',
+                    controllerAs: 'deleteModelCtrl',
+                    size: 'lg',
+                    appendTo: parentElem,
+                    resolve: {
+                        id: function () {
+                            return id;
+                        },
+                        refresh:function () {
 
-        main.dtColumns = [
-            DTColumnBuilder.newColumn('name').withTitle('Name').withOption('defaultContent', '-'),
-            DTColumnBuilder.newColumn('publishYear').withTitle('Publish Year')
-        ];
+                        }
+                    }
+                });
+                modalInstance.result.then(function (success) {
+                    if (success) {
+                        main.dtInstance._renderer.rerender();
+                    }
+                })
+            };
 
-        main.hideSearch = true;
-        main.searchObject = {
-            name: null,
-            publishYear: null
+            main.dtInstanceCallback = function (dtInstance) {
+                main.dtInstance = dtInstance;
+                dtInstance.DataTable.on('draw.dt', function () {
+                    var elements = angular.element("#" + dtInstance.id + " .ng-scope");
+                    angular.forEach(elements, function (element) {
+                        $compile(element)($scope);
+                    });
+                });
+            };
+
+            main.hideSearch = true;
+            main.searchObject = {
+                name: null,
+                publishYear: null
+            };
+
+        }
+    );
+angular.module('Book')
+    .controller('DeleteModelCtrl', function ($uibModalInstance, $http, id, refresh) {
+        var deleteModelCtrl = this;
+        deleteModelCtrl.ok = function () {
+            $http.delete('cxf/rest/Book/' + id).then(function (result) {
+                $uibModalInstance.close(true);
+            });
         };
-
+        deleteModelCtrl.cancel = function () {
+            $uibModalInstance.close(false);
+        };
     });
