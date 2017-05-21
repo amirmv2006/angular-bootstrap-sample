10caca058f5a617e1a335868a0768511f03fdf9c
Amir MV <amirmv2006@gmail.com>
2017-05-21 14:31:52 +0430

    -added base edit controller

--- src/main/webapp/amv/book/bookedit/controller/BookEditController.js
+++ src/main/webapp/amv/book/bookedit/controller/BookEditController.js
@@ -1,54 +1,9 @@
 angular.module('Book')
     .controller('BookEditPageCtrl', function ($scope, $location, $controller, $log, $http, $q,
-                                              BasePageController, PageAction, NotificationService, NavigationService, BookService
-                                              ) {
-        var main = this;
-        main = angular.extend(main, new BasePageController($scope, $location, $controller, "BookAdd"));
-        var bookAddPage = main.page;
-        bookAddPage.pageParameters = {
-            id:null
-        };
-        bookAddPage.addAction(new PageAction("Save", "fa fa-save", function () {
-            console.log("Saving");
-            main.save();
-        }));
-        console.log("initializing Book model");
-        main.bookForm = {};
+                                              BaseEditController, PageAction, NotificationService, NavigationService,
+                                              BookService, BookModel) {
+        var main = angular.extend(this, new BaseEditController($scope, $location, $controller,
+            "BookAdd", "/book", "bookEditPageCtrl",
+            BookService, BookModel));
 
-        main.book = {};
-
-        var id = $location.search().id;
-        if (id) {
-            BookService.getById(id, function (result) {
-                angular.copy(result, main.book);
-            });
-        }
-
-        main.resetForm = function () {
-            main.book.name = null;
-            main.book.publishYear = 0;
-        };
-
-
-        main.save = function () {
-            console.log(main.book);
-            // if (main.bookForm.$valid) {
-            if (main.book.id) {
-                BookService.update(angular.copy(main.book), function (result) {
-                    main.resetForm();
-                    $log.debug('RESULT', result);
-                    NavigationService.goBack();
-                });
-            } else {
-                BookService.save(angular.copy(main.book), function (result) {
-                    main.resetForm();
-                    $log.debug('RESULT', result);
-                    NavigationService.goBack();
-                });
-            }
-            // } else {
-            //     console.log('Invalid');
-            // }
-        };
-        main.resetForm();
     });
